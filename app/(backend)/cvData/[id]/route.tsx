88ae2830d690537b5f729cdbd9430105b1c2e866
Daniel <daniel_ciecior@yahoo.de>
2025-09-27 15:13:45 +0200

    bugfix build errors

--- app/(backend)/cvData/[id]/route.tsx
+++ app/(backend)/cvData/[id]/route.tsx
@@ -10,7 +10,7 @@ import { deleteCV } from "../functions/deleteCV";
 
 export async function GET(
   request: NextRequest,
-  { params }: { params: { id: string } }) {
+  { params }: { params: Promise<{ id: string }> }) {
     try {
         const supabase: SupabaseClient = await createClient();
         const { id } = await params;
@@ -38,7 +38,7 @@ export async function GET(
 
 export async function PUT(
   request: NextRequest,
-  { params }: { params: { id: string } }) {
+  { params }: { params: Promise<{ id: string }> }) {
     try {
         const supabase = await createClient();
         const { id } = await params;
@@ -67,7 +67,7 @@ export async function PUT(
 
 export async function PATCH(
   request: NextRequest,
-  { params }: { params: { id: string } }) {
+  { params }: { params: Promise<{ id: string }> }) {
     try {
         const supabase = await createClient();
         const { id } = await params;
@@ -96,7 +96,7 @@ export async function PATCH(
 
 export async function POST(
   request: NextRequest,
-  { params }: { params: { id: string } }) {
+  { params }: { params: Promise<{ id: string }> }) {
     try {
         const supabase = await createClient();
         const { id } = await params;
@@ -124,7 +124,7 @@ export async function POST(
 
 export async function DELETE(
   request: NextRequest,
-  { params }: { params: { id: string } }) {
+  { params }: { params: Promise<{ id: string }> }) {
     try {
         const supabase = await createClient();
         const { id } = await params;
